375a94327f6515fc3db408389377aeed8f5d417a
HaeTi <t9h9@naver.com>
2023-11-15 16:41:17 +0900

    Alignment

--- app/src/main/java/org/haeti/composebasic/MainActivity.kt
+++ app/src/main/java/org/haeti/composebasic/MainActivity.kt
@@ -5,14 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -27,7 +25,13 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Column(Modifier.background(Color.Blue).padding(16.dp)) {
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .background(Color.Blue)
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
                         Text("Hello")
                         Text("World")
                     }
