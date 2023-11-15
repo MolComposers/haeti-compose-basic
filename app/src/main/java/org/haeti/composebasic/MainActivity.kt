9e34b56d922af803ad5b8562d5588105e9f0657b
HaeTi <t9h9@naver.com>
2023-11-15 17:03:14 +0900

    Box height

--- app/src/main/java/org/haeti/composebasic/MainActivity.kt
+++ app/src/main/java/org/haeti/composebasic/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Box(
                 Modifier.background(Color.Green)
-                    .fillMaxSize(),
+                    .fillMaxWidth().height(200.dp),
             ) {
                 Text("Hello")
                 Box(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().padding(16.dp),
                     contentAlignment = Alignment.BottomEnd,
                 ) {
                     Text("DODODODODODO")
