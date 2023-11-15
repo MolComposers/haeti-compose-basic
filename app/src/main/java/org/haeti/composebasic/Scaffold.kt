78da573c314209d0cfd5a389dc8039bc9db36d3e
HaeTi <t9h9@naver.com>
2023-11-16 02:54:51 +0900

    basic Scaffold

---
+++ app/src/main/java/org/haeti/composebasic/Scaffold.kt
@@ -0,0 +1,50 @@
+package org.haeti.composebasic
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class Scaffold : androidx.activity.ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            scaffold()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun scaffold() {
+    val textValue = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        TextField(
+            value = textValue.value,
+            onValueChange = {
+                textValue.value = it
+            },
+        )
+        Button(onClick = { }) {
+            Text(text = "클릭!")
+        }
+    }
+}
