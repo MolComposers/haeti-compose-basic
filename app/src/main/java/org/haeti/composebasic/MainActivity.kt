8faea728e45e74b25f2459b70aafefa21eb574ca
HaeTi <t9h9@naver.com>
2023-11-15 17:16:26 +0900

    list from column with scrollState

--- app/src/main/java/org/haeti/composebasic/MainActivity.kt
+++ app/src/main/java/org/haeti/composebasic/MainActivity.kt
@@ -4,31 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                Modifier.background(Color.Green)
-                    .fillMaxWidth().height(200.dp),
+            val scrollState = rememberScrollState()
+
+            Column(
+                Modifier.fillMaxWidth().background(Color.Green).verticalScroll(scrollState),
             ) {
-                Text("Hello")
-                Box(
-                    modifier = Modifier.fillMaxSize().padding(16.dp),
-                    contentAlignment = Alignment.BottomEnd,
-                ) {
-                    Text("DODODODODODO")
+                for (i in 1..50) {
+                    Text("글씨 $i")
                 }
             }
         }
