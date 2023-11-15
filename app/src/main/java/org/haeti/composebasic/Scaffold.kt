dc0a8f1709ce3b0c086e0cf71bf9a86332a2801f
HaeTi <t9h9@naver.com>
2023-11-16 03:17:44 +0900

    hide keyboard when touch button

--- app/src/main/java/org/haeti/composebasic/Scaffold.kt
+++ app/src/main/java/org/haeti/composebasic/Scaffold.kt
@@ -5,44 +5,66 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 class Scaffold : androidx.activity.ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            scaffold()
+            customScaffold()
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
-fun scaffold() {
+fun customScaffold() {
     val (text, setValue) = remember {
         mutableStateOf("")
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        TextField(
-            value = text,
-            onValueChange = setValue,
-        )
-        Button(onClick = { }) {
-            Text(text = "클릭!")
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(contentPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TextField(
+                value = text,
+                onValueChange = setValue,
+            )
+            Button(onClick = {
+                keyboardController?.hide()
+                scope.launch {
+                    snackbarHostState.showSnackbar("Helloe $text")
+                }
+            }) {
+                Text(text = "클릭!")
+            }
         }
     }
 }
