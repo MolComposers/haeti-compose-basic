5ee914456feb525554c6bab7ad6ffe732a793372
HaeTi <t9h9@naver.com>
2023-11-15 17:20:47 +0900

    list from lazy column

--- app/src/main/java/org/haeti/composebasic/MainActivity.kt
+++ app/src/main/java/org/haeti/composebasic/MainActivity.kt
@@ -4,25 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scrollState = rememberScrollState()
-
-            Column(
-                Modifier.fillMaxWidth().background(Color.Green).verticalScroll(scrollState),
+            LazyColumn(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.Green),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
             ) {
-                for (i in 1..50) {
-                    Text("글씨 $i")
+                items(50) { index ->
+                    Text(text = "글씨 $index")
                 }
             }
         }
