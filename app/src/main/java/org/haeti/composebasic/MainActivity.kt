e8680c3a84f5825366822b9e2495a70f259cedc2
HaeTi <t9h9@naver.com>
2023-11-15 22:48:20 +0900

    Card 추가 rememberSaveable로 상태보관 modifier 외부에서 지정 콜백을 통해 버튼 상태 바꾸기

--- app/src/main/java/org/haeti/composebasic/MainActivity.kt
+++ app/src/main/java/org/haeti/composebasic/MainActivity.kt
@@ -3,35 +3,82 @@ package org.haeti.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(
-                Modifier
-                    .fillMaxWidth()
-                    .background(Color.Green),
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
+            var isFovorite by rememberSaveable {
+                mutableStateOf(false)
+            }
+
+            imageCard(
+                modifier = Modifier.fillMaxWidth(0.5f)
+                    .padding(16.dp),
+                isFovorite = isFovorite,
+            ) { favorite ->
+                isFovorite = favorite
+            }
+        }
+    }
+}
+
+@Composable
+fun imageCard(
+    modifier: Modifier = Modifier,
+    isFovorite: Boolean,
+    onTabFovorite: (Boolean) -> Unit,
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp,
+        ),
+    ) {
+        Box(modifier = Modifier.height(180.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_snim),
+                contentDescription = "티뱃여우",
+                contentScale = ContentScale.Crop,
+            )
+            Box(
+                Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
             ) {
-                item {
-                    Text("Header")
-                }
-                items(50) { index ->
-                    Text(text = "글씨 $index")
-                }
-                item {
-                    Text("Footer")
+                IconButton(onClick = {
+                    onTabFovorite(!isFovorite)
+                }) {
+                    Icon(
+                        imageVector = if (isFovorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White,
+                    )
                 }
             }
         }
