59c5d5f814f821eead5b03cac571f8923552225e
HaeTi <t9h9@naver.com>
2023-11-15 17:01:50 +0900

    Box

--- app/src/main/java/org/haeti/composebasic/MainActivity.kt
+++ app/src/main/java/org/haeti/composebasic/MainActivity.kt
@@ -4,37 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import org.haeti.composebasic.ui.theme.ComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBasicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    color = MaterialTheme.colorScheme.background,
+            Box(
+                Modifier.background(Color.Green)
+                    .fillMaxSize(),
+            ) {
+                Text("Hello")
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.BottomEnd,
                 ) {
-                    Column(
-                        Modifier
-                            .fillMaxSize()
-                            .background(Color.Blue)
-                            .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Text("Hello")
-                        Text("World")
-                    }
+                    Text("DODODODODODO")
                 }
             }
         }
