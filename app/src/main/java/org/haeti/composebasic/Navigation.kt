17b7dd56cbbfe2cfbb0591a29bdeae25902dc50a
HaeTi <t9h9@naver.com>
2023-11-23 00:03:04 +0900

    첫 화면 구성

--- app/src/main/java/org/haeti/composebasic/Navigation.kt
+++ app/src/main/java/org/haeti/composebasic/Navigation.kt
@@ -3,7 +3,20 @@ package org.haeti.composebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -31,6 +44,26 @@ class Navigation : ComponentActivity() {
 
 @Composable
 fun FirstScreen() {
+    val (value, setValue) = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "첫 화면")
+        Spacer(Modifier.height(16.dp))
+        Button(onClick = { }) {
+            Text(text = "두 번째 화면으로")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(value = value, onValueChange = setValue)
+        Button(onClick = { }) {
+            Text(text = "세 번째 화면으로")
+        }
+    }
 }
 
 @Composable
