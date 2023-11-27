22e168b6aac44b5300cbdf1e94f2e95beecd9e70
HaeTi <t9h9@naver.com>
2023-11-27 10:42:12 +0900

    ADD Bmi module

---
+++ Bmicalculator/src/main/java/org/haeti/bmicalculator/MainActivity.kt
@@ -0,0 +1,35 @@
+package org.haeti.bmicalculator
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "BMI Calculator") }
+            )
+        }
+    ) {
+
+    }
+}
