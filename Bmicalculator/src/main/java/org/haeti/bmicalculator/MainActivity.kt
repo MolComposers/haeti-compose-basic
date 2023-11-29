eadd994387f93b2919f9682a14a2201523e6657a
HaeTi <t9h9@naver.com>
2023-11-29 10:42:12 +0900

    OutlinedTextField

--- Bmicalculator/src/main/java/org/haeti/bmicalculator/MainActivity.kt
+++ Bmicalculator/src/main/java/org/haeti/bmicalculator/MainActivity.kt
@@ -4,11 +4,17 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +32,25 @@ fun HomeScreen() {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "BMI Calculator") }
+                title = { Text(text = "BMI Calculator") },
+            )
+        },
+    ) {
+        Column() {
+            OutlinedTextField(
+                value = " ",
+                onValueChange = {},
+                label = { Text("키") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            OutlinedTextField(
+                value = " ",
+                onValueChange = {},
+                label = { Text("몸무게") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             )
         }
-    ) {
-
     }
 }
