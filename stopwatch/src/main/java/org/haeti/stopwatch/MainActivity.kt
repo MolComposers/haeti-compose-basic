9e160e3506169b4702644358970c68a8675940d2
HaeTi <t9h9@naver.com>
2023-12-31 01:57:35 +0900

    [스톱워치] UI 구현

--- stopwatch/src/main/java/org/haeti/stopwatch/MainActivity.kt
+++ stopwatch/src/main/java/org/haeti/stopwatch/MainActivity.kt
@@ -1,10 +1,34 @@
 package org.haeti.stopwatch
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import java.util.Timer
 import kotlin.concurrent.timer
@@ -66,5 +90,87 @@ class MainViewModel : ViewModel() {
     fun recordLapTime() {
         _lapTimes.value.add(0, "$lap LAP: ${sec.value}.${milliSec.value}")
         lap++
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen(
+    sec: Int,
+    milliSec: Int,
+    isRunning: Boolean,
+    lapTimes: List<String>,
+    onToggle: (Boolean) -> Unit,
+    onReset: () -> Unit,
+    onLapTime: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("StopWatch") })
+        },
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row(
+                verticalAlignment = Alignment.Bottom,
+            ) {
+                Text(text = "$sec", fontSize = 100.sp)
+                Text(text = "$milliSec")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState()),
+            ) {
+                lapTimes.forEach { lapTime ->
+                    Text(text = lapTime)
+                }
+            }
+
+            Row(
+                modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                FloatingActionButton(
+                    onClick = { onReset() },
+                    containerColor = androidx.compose.ui.graphics.Color.Red,
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_baseline_refresh_24),
+                        contentDescription = "reset",
+                    )
+                }
+
+                FloatingActionButton(
+                    onClick = { onToggle(isRunning) },
+                    containerColor = androidx.compose.ui.graphics.Color.Green,
+                ) {
+                    Image(
+                        painter = painterResource(
+                            id =
+                            if (isRunning) {
+                                R.drawable.ic_baseline_pause_24
+                            } else {
+                                R.drawable.ic_baseline_play_arrow_24
+                            },
+                        ),
+                        contentDescription = "start/pause",
+                    )
+                }
+
+                Button(onClick = { onLapTime() }) {
+                    Text("랩 타임")
+                }
+            }
+        }
     }
 }
