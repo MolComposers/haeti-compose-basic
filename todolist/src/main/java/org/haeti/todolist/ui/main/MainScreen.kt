dd75b982c190623220eeead3ed251e11bef38892
HaeTi <t9h9@naver.com>
2024-02-09 21:33:26 +0900

    [Todo List] 할 일 추가 텍스트 필드

---
+++ todolist/src/main/java/org/haeti/todolist/ui/main/MainScreen.kt
@@ -0,0 +1,77 @@
+package org.haeti.todolist.ui.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import org.haeti.todolist.R
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@Composable
+fun MainScreen(
+    viewModel: MainViewModel
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    var text by rememberSaveable { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("오늘 할 일") }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxSize(),
+                shape = RoundedCornerShape(8.dp),
+                placeholder = { Text("할 일을 입력하세요.") },
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_add_24),
+                        contentDescription = "Add Todo",
+                    )
+                },
+                maxLines = 1,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    viewModel.addTodo(text = text)
+                    text = ""
+                    keyboardController?.hide()
+                })
+            )
+        }
+    }
+}
