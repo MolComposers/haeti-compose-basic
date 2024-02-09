fa67b3fc95a3eb1eab859b69d34db62bf0461f07
HaeTi <t9h9@naver.com>
2024-02-09 21:33:45 +0900

    [Todo List] Divider 추가

--- todolist/src/main/java/org/haeti/todolist/ui/main/MainScreen.kt
+++ todolist/src/main/java/org/haeti/todolist/ui/main/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -72,6 +73,8 @@ fun MainScreen(
                     keyboardController?.hide()
                 })
             )
+
+            Divider()
         }
     }
 }
