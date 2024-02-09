25081d143c4d3aa44a70d62b8b0a72ede4a2ef36
HaeTi <t9h9@naver.com>
2024-02-09 21:55:52 +0900

    [Todo List] SnackBar 띄우기 구현

--- todolist/src/main/java/org/haeti/todolist/ui/main/MainScreen.kt
+++ todolist/src/main/java/org/haeti/todolist/ui/main/MainScreen.kt
@@ -17,11 +17,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -31,6 +35,7 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import org.haeti.todolist.R
 import org.haeti.todolist.ui.main.components.TodoItem
 
@@ -39,6 +44,9 @@ import org.haeti.todolist.ui.main.components.TodoItem
 fun MainScreen(
     viewModel: MainViewModel
 ) {
+    var snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     val keyboardController = LocalSoftwareKeyboardController.current
 
     var text by rememberSaveable { mutableStateOf("") }
@@ -96,9 +104,18 @@ fun MainScreen(
                                 viewModel.toggle(todo.uniqueId)
                             },
                             onDeleteClick = { todo ->
-                                viewModel.deleteTodo(
-                                    uniqueId = todo.uniqueId
-                                )
+                                viewModel.deleteTodo(uniqueId = todo.uniqueId)
+
+                                scope.launch {
+                                    val result = snackbarHostState.showSnackbar(
+                                        message = "할 일이 삭제되었습니다.",
+                                        actionLabel = "취소",
+                                    )
+
+                                    if (result == SnackbarResult.ActionPerformed) {
+                                        viewModel.restoreTodo()
+                                    }
+                                }
                             }
                         )
                         Spacer(modifier = Modifier.height(16.dp))
