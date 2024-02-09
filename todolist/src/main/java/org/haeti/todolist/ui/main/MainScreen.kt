03f5216e869ea52018f08a4ade70573aee9a2c5d
HaeTi <t9h9@naver.com>
2024-02-09 21:50:50 +0900

    [Todo List] LazyColumn 추가

--- todolist/src/main/java/org/haeti/todolist/ui/main/MainScreen.kt
+++ todolist/src/main/java/org/haeti/todolist/ui/main/MainScreen.kt
@@ -1,8 +1,14 @@
 package org.haeti.todolist.ui.main
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -20,11 +26,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import org.haeti.todolist.R
+import org.haeti.todolist.ui.main.components.TodoItem
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
@@ -75,6 +83,30 @@ fun MainScreen(
             )
 
             Divider()
+
+            LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(viewModel.items.value) { todoItem ->
+                    Column {
+                        TodoItem(
+                            todo = todoItem,
+                            onClick = { todo ->
+                                viewModel.toggle(todo.uniqueId)
+                            },
+                            onDeleteClick = { todo ->
+                                viewModel.deleteTodo(
+                                    uniqueId = todo.uniqueId
+                                )
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Divider(color = Color.Black, thickness = 1.dp)
+                    }
+
+                }
+            }
         }
     }
 }
