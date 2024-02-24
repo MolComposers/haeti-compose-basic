0fdd91ab89c9a7e6e719c37283b5aa42386b1c91
HaeTi <t9h9@naver.com>
2024-02-24 10:55:12 +0900

    string 추출

--- todolist/src/main/java/org/haeti/todolist/ui/main/components/TodoItem.kt
+++ todolist/src/main/java/org/haeti/todolist/ui/main/components/TodoItem.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
@@ -25,7 +26,7 @@ fun TodoItem(
     onClick: (todo: Todo) -> Unit = {},
     onDeleteClick: (todo: Todo) -> Unit = {},
 ) {
-    val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val format = SimpleDateFormat(stringResource(R.string.date_format_std), Locale.getDefault())
 
     Row(
 
